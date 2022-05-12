8eaa6d27fe5107deba20259c85c96e3843cda975
alimtegar <alimtegar4@gmail.com>
2022-05-12 07:05:48 +0700

    Create prototype of VLAB

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useCallback } from 'react'
+import { Math as ThreeMath } from 'three'
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useSpring, useTransition, config, animated } from '@react-spring/three';
+import { useDrag } from '@use-gesture/react';
 
-function App() {
+const { degToRad } = ThreeMath
+
+const Pyramid = ({ geometryArgs = [2, 2, 4, 1], rotation, ...props }) => {
+  const mesh = useRef(null)
+
+  useFrame((state, delta) => (mesh.current.rotation.y += 0.01));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <animated.mesh
+      {...props}
+      ref={mesh}
+      scale={1}
+      rotation={rotation.to((x, y, z) => [degToRad(x), degToRad(y), degToRad(z)])}
+    >
+      <coneGeometry args={geometryArgs} />
+      <meshStandardMaterial color="orange" />
+    </animated.mesh>
+  );
+};
+
+const App = () => {
+  const [{ rotation }, setRotation] = useSpring(() => ({
+    rotation: [0, 0, 0],
+    config: { mass: 10, tension: 1000, friction: 300, precision: 0.00001 }
+  }))
+
+  const bind = useDrag(({ delta, velocity, direction, memo = rotation.get() }) => {
+    const x = memo[0] + (delta[1] / window.innerWidth) * 180;
+    const y = memo[1] + (delta[0] / window.innerHeight) * 180;
+    const vxyz = [direction[1] * (velocity[1] / 1), direction[0] * (velocity[0] / 1), 0];
+
+    setRotation({
+      rotation: [x, y, 0],
+      config: { velocity: vxyz, decay: true }
+    });
+
+    return memo;
+  });
+
+  return (
+    <div {...bind()} style={{ touchAction: 'none' }}>
+      <Canvas style={{ backgroundColor: '#eeeeee', height: '50vh', }}>
+        <ambientLight color="#888888" />
+        <pointLight position={[10, 20, 0]} />
+        <Pyramid rotation={rotation} />
+      </Canvas>
     </div>
   );
-}
+};
 
 export default App;
